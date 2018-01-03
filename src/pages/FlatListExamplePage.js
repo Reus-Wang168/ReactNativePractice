c48d3b73c586241556dc97f0b97edeaaba8ee7fe
REBOOTERS <foreverlove.zyl@163.com>
2018-01-03 18:26:05 +0800

    flatlist exchange

--- src/pages/FlatListExamplePage.js
+++ src/pages/FlatListExamplePage.js
@@ -3,11 +3,13 @@
  */
 
 import React, {Component} from 'react';
-import {View, Text, FlatList, ScrollView} from 'react-native';
+import {View, Text, FlatList, ScrollView, TouchableOpacity, Dimensions} from 'react-native';
 
 import Icon from '../component/icon/icon';
 
-const data = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+const width = Dimensions.get('window').width;
+
+const data = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
 
 class Header extends Component {
 
@@ -29,7 +31,9 @@ class Header extends Component {
 
         return (<View style={{height: 200, backgroundColor: '#03A9F4'}}>
             <ScrollView contentContainerStyle={{alignItems: 'center'}}>
-                {renderMyView()}
+                {/*{renderMyView()}*/}
+                <Text style={{margin: 30, fontSize: 20, color: 'white'}}>Head</Text>
+
             </ScrollView>
 
         </View>)
@@ -46,7 +50,7 @@ class Footer extends Component {
 
 export default class FlatListExample extends Component {
     static navigationOptions = ({navigation}) => ({
-        title: navigation.state.params.title
+        title: navigation.state.params.title,
     });
 
     constructor(props) {
@@ -54,17 +58,37 @@ export default class FlatListExample extends Component {
 
         this.state = ({
             refreshing: false,
+            num: 1
         });
 
     }
 
+    _onPress = () => {
+        if (this.state.num === 1) {
+            this.setState({
+                num: 2
+            })
+        } else {
+            this.setState({
+                num: 1
+            })
+        }
+
+
+    };
+
 
     _keyExtractor = (item, index) => item;
     _renderItem = ({item}) => (
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text style={{padding: 10, fontSize: 18, height: 44, margin: 10}}>{item}</Text>
-            <Icon name={'ios-share'}/>
-        </View>
+        <TouchableOpacity onPress={this._onPress}>
+            <View style={{flexDirection: 'row', alignItems: 'center', backgroundColor: '#4bd8d9', width: width/2}}>
+
+                <Text style={{padding: 10, fontSize: 18, height: 44, margin: 10}}>{item}</Text>
+                <Icon name={'ios-share'}/>
+
+
+            </View>
+        </TouchableOpacity>
 
     );
     _onEndReached = ({info}) => {
@@ -87,9 +111,17 @@ export default class FlatListExample extends Component {
     render() {
         return (<View style={{flex: 1}}>
             <FlatList
+                // columnWrapperStyle={this.state.num > 1 ? {
+                //     flexDirection: 'row',
+                //     alignItems: 'center',
+                //     width: width / 2,
+                //     backgroundColor: '#d9373e'
+                // } : null}
+                numColumns={this.state.num}
                 onRefresh={this._onRefresh}
                 refreshing={this.state.refreshing}
                 data={data}
+                key={this.state.num}
                 keyExtractor={this._keyExtractor}
                 renderItem={(item) => this._renderItem(item)}
                 ListHeaderComponent={Header}
