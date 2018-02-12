e917682dee78605415afe88f7b0076d60c5d4e45
REBOOTERS <foreverlove.zyl@163.com>
2018-02-12 12:00:24 +0800

    update views

--- android/app/src/main/java/com/reactnativepractice/MainApplication.java
+++ android/app/src/main/java/com/reactnativepractice/MainApplication.java
@@ -7,6 +7,7 @@ import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.reactnativepractice.custom.CustomPackage;
 import com.rookie.customscrollview.CustomScrollViewPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -19,39 +20,41 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new RNFetchBlobPackage(),
+                new VectorIconsPackage(),
+                new LinearGradientPackage(),
+                new PickerPackage(),
+                new CustomScrollViewPackage(),
+                new RCTCameraPackage(),
+                new CustomPackage(),
+                new CustomScrollViewPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFetchBlobPackage(),
-            new VectorIconsPackage(),
-            new LinearGradientPackage(),
-            new PickerPackage(),
-            new CustomScrollViewPackage(),
-            new RCTCameraPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
---
+++ android/app/src/main/java/com/reactnativepractice/customview/ReactMyViewPagerManager.java
@@ -0,0 +1,52 @@
+package com.reactnativepractice.customview;
+
+import android.view.View;
+
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+
+/**
+ * Created by Rookie on 2017/8/29.
+ */
+
+public class ReactMyViewPagerManager extends ViewGroupManager<MyReactViewPager> {
+
+
+    private static final String REACT_CLASS = "MyAndroidViewPager";
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    protected MyReactViewPager createViewInstance(ThemedReactContext reactContext) {
+        return new MyReactViewPager(reactContext);
+    }
+
+
+    @Override
+    public boolean needsCustomLayoutForChildren() {
+        return true;
+    }
+
+    @Override
+    public void addView(MyReactViewPager parent, View child, int index) {
+        parent.addViewToAdapter(child, index);
+    }
+
+    @Override
+    public int getChildCount(MyReactViewPager parent) {
+        return parent.getViewCountInAdapter();
+    }
+
+    @Override
+    public View getChildAt(MyReactViewPager parent, int index) {
+        return parent.getViewFromAdapter(index);
+    }
+
+    @Override
+    public void removeViewAt(MyReactViewPager parent, int index) {
+        parent.removeViewFromAdapter(index);
+    }
+}
